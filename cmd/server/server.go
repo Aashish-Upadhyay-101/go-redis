0afd132d0dbbcd817d1654b4e8e6d53ea7932699
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2025-01-10 21:02:27 +0000

    fix: refactor a little bit

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,25 +3,61 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
+)
+
+var (
+	listen = flag.String("listen", "127.0.0.1:6397", "address to listen to")
 )
 
 func main() {
 	flag.Parse()
 
-	listener, err := net.Listen("tcp", "127.0.0.1:6379")
-	if err != nil { 
-		fmt.Println("Failed to connect to port 6379")
-		os.Exit(1)	
-	}
-
-	conn, err := listener.Accept()
+	err := run()
 	if err != nil {
-		fmt.Println("Failed to accept connection: ", err.Error())
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
-
-	conn.Write([]byte("+PONG\r\n"))
 }
 
+func run() (err error) {
+	l, err := net.Listen("tcp", *listen)
+	if err != nil {
+		return fmt.Errorf("listen: %w", err)
+	}
+
+	defer closeIt(l, &err, "close listener")
+
+	c, err := l.Accept()
+	if err != nil {
+		return fmt.Errorf("accept: %w", err)
+	}
+
+	defer closeIt(c, &err, "close connection")
+
+	buf := make([]byte, 128)
+
+	_, err = c.Read(buf)
+	if err != nil {
+		return fmt.Errorf("read command: %w", err)
+	}
+
+	log.Printf("read command:\n%s", buf)
+
+	_, err = c.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		return fmt.Errorf("write response: %w", err)
+	}
+
+	return nil
+}
+
+func closeIt(c io.Closer, errp *error, msg string) {
+	err := c.Close()
+	if *errp == nil {
+		*errp = fmt.Errorf("%v: %w", msg, err)
+	}
+}
